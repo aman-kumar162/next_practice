2366549f778ca3e2ee7f87040fdab4ab48b637b1
Aman kumar <116007359+aman-kumar162@users.noreply.github.com>
2024-03-15 15:35:39 +0000

    worng path

--- next_feature/basic-app/src/components/Navbar.tsx
+++ next_feature/basic-app/src/components/Navbar.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+'use client';
 
-function Navbar(){
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from 'next/link';
+
+
+function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null);
+
     return (
-        <div>
+        <div
+        >
             this is nav bar
         </div>
     )
